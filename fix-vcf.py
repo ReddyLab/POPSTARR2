f23e0e3fe41fe3358d12fb7afd313120feb803ac
Bill Majoros <martiandna@gmail.com>
2017-06-19 14:54:31 -0400

    update

--- fix-vcf.py
+++ fix-vcf.py
@@ -2,7 +2,6 @@
 #=========================================================================
 # This is OPEN SOURCE SOFTWARE governed by the Gnu General Public
 # License (GPL) version 3, as described at www.opensource.org.
-# Copyright (C)2017 William H. Majoros (martiandna@gmail.com).
 #=========================================================================
 from __future__ import (absolute_import, division, print_function, 
    unicode_literals, generators, nested_scopes, with_statement)
